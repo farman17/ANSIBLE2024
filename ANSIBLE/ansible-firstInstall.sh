abcf99578b349b9481b94519791ebda65cb7b294
farman17 <ahmadfarman1701@gmail.com>
2024-08-28 19:41:10 +0700

    add file

--- ANSIBLE/ansible-firstInstall.sh
+++ ANSIBLE/ansible-firstInstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 clear
+echo "PROSES INI HANYA DILAKUKAN DI MESIN BASTION. UNTUK INSTALL ANSIBLE DAN DEPENDENSI NYA"
+echo
+sudo apt install figlet
+figlet DEVOPS
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 apt-get update
@@ -8,22 +12,23 @@ sudo apt install -y ansible
 #apt-get install python3 ansible
 clear
 
-echo -n "masukkan username baru-ansible: ";
-read username;
-adduser $username
-usermod -aG sudo $username
+#echo -n "masukkan username baru-ansible: ";
+#read username;
+#adduser $username
+#usermod -aG sudo $username
 
-runuser -l  $username  -c  'sudo ssh-keygen -t rsa'
-runuser -l  $username  -c  'sudo chmod 400 /root/.ssh/id_rsa'
+#runuser -l  $username  -c  'sudo ssh-keygen -t rsa'
+#runuser -l  $username  -c  'sudo chmod 400 /root/.ssh/id_rsa'
+
 #sudo ssh-keygen -t rsa
 #cd /root/.ssh
 #sudo chmod 400 id_rsa
 
-echo -n "masukkan domain dest-host: ";
+echo -n "masukkan domain untuk config ansible (free): ";
 read host1;
 echo -n "masukkan IP dest-host: ";
 read ip1;
-echo -n "masukkan username dest-host: ";
+echo -n "masukkan user dest-host: ";
 read user1;
 
 echo
@@ -39,4 +44,4 @@ echo
 echo
 ansible $host1 -m ping
 echo
-echo "DONE....ANSIBLE username anda [$username] telah berhasil terkoneksi dengan user $user1 dengan hostname  $host1 dan ip-address $ip1.."
+echo "DONE....ANSIBLE BERHASIL DI INSTALL SELANJUTNYA JANGAN LUPA domain dan user disesuaikan di file yml.."
